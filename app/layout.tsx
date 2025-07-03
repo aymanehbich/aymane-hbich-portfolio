caf6e6afc482137423b08c7447b65197ae23fe63
hbich aymane <aymangame077@gmail.com>
2025-07-03 21:58:29 +0100

    Codebase

---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+import "@mantine/core/styles.css";
+import React from "react";
+import {
+  MantineProvider,
+  ColorSchemeScript,
+  mantineHtmlProps,
+} from "@mantine/core";
+import { theme } from "../theme";
+
+export const metadata = {
+  title: "aymane hbich",
+  description:
+    "This is my portfolio website, showcasing my projects and skills.",
+};
+
+export default function RootLayout({ children }: { children: any }) {
+  return (
+    <html lang="en" {...mantineHtmlProps}>
+      <head>
+        <ColorSchemeScript />
+        <link rel="shortcut icon" href="/favicon.svg" />
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
+        />
+      </head>
+      <body>
+        <MantineProvider theme={theme}>{children}</MantineProvider>
+      </body>
+    </html>
+  );
+}
