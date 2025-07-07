008aa57a2603196e921b2acb6cb4ec98bcc51a8a
hbich aymane <aymangame077@gmail.com>
2025-07-08 00:23:25 +0100

    feat : navbar and header components.

---
+++ app/Components/Header/Header.tsx
@@ -0,0 +1,101 @@
+import {
+  alpha,
+  AppShell,
+  Burger,
+  Button,
+  Group,
+  UnstyledButton,
+  useMantineTheme,
+} from "@mantine/core";
+import Link from "next/link";
+import classes from "./Header.module.css";
+import IconArrowRight from "@tabler/icons-react/dist/esm/icons/IconArrowRight";
+import { AYMANE_LOGO } from "../Logo/Logo";
+import { useWindowScroll } from "@mantine/hooks";
+import clsx from "clsx";
+import { MantineLogo } from "@mantinex/mantine-logo";
+
+type HeaderProps = {
+  opened: boolean;
+  toggle: () => void;
+};
+export default function Header({ opened, toggle }: HeaderProps) {
+  const [scroll] = useWindowScroll();
+  const scrollYMax = 20; // Define the scroll threshold for header style change
+  return (
+    <AppShell.Header
+      className={clsx(
+        classes.header,
+        scroll.y > scrollYMax && classes.headerScrolled
+      )}
+    >
+      <Group h="100%" px="md">
+        <Burger opened={opened} onClick={toggle} hiddenFrom="md" size="sm" />
+        <Group justify="space-between" style={{ flex: 1 }}>
+          {/* <UnstyledButton display="flex" component={Link} href="/">
+            <MantineLogo size={30} />
+          </UnstyledButton> */}
+          <UnstyledButton display="flex" component={Link} href="/">
+            <AYMANE_LOGO size={25} />
+          </UnstyledButton>
+          <Group
+            visibleFrom="md"
+            className={clsx(
+              classes.linksWrapper,
+              scroll.y > scrollYMax && classes.scrollModifier
+            )}
+          >
+            <UnstyledButton
+              component={Link}
+              href="/#about"
+              className={classes.link}
+            >
+              About
+            </UnstyledButton>
+            <UnstyledButton
+              component={Link}
+              href="/#skills"
+              className={classes.link}
+            >
+              Skills
+            </UnstyledButton>
+            <UnstyledButton
+              component={Link}
+              href="/#education"
+              className={classes.link}
+            >
+              Education
+            </UnstyledButton>
+            <UnstyledButton
+              component={Link}
+              href="/#experience"
+              className={classes.link}
+            >
+              Experience
+            </UnstyledButton>
+            <UnstyledButton
+              component={Link}
+              href="/#projects"
+              className={classes.link}
+            >
+              Projects
+            </UnstyledButton>
+          </Group>
+          <Button
+            component={Link}
+            href="#contact"
+            variant="outline"
+            // gradient={{ from: "myColor", to: "red" }}
+            radius="xl"
+            // color="myColor"
+            size="md"
+            rightSection={<IconArrowRight size={16} />}
+            // className={classes.connectButton}
+          >
+            Connect
+          </Button>
+        </Group>
+      </Group>
+    </AppShell.Header>
+  );
+}
---
+++ app/Components/Navbar/Navbar.tsx
@@ -0,0 +1,54 @@
+import { AppShell, Button, Group, UnstyledButton } from "@mantine/core";
+import React from "react";
+import classes from "./Navbar.module.css";
+import Link from "next/link";
+import { IconArrowRight } from "@tabler/icons-react";
+import { useWindowScroll } from "@mantine/hooks";
+import clsx from "clsx";
+
+export default function Navbar() {
+  const [scroll] = useWindowScroll();
+  return (
+    <AppShell.Navbar py="md" px={4}>
+      <UnstyledButton component={Link} href="/#about" className={classes.link}>
+        About
+      </UnstyledButton>
+      <UnstyledButton component={Link} href="/#skills" className={classes.link}>
+        Skills
+      </UnstyledButton>
+      <UnstyledButton
+        component={Link}
+        href="/#education"
+        className={classes.link}
+      >
+        Education
+      </UnstyledButton>
+      <UnstyledButton
+        component={Link}
+        href="/#experience"
+        className={classes.link}
+      >
+        Experience
+      </UnstyledButton>
+      <UnstyledButton
+        component={Link}
+        href="/#projects"
+        className={classes.link}
+      >
+        Projects
+      </UnstyledButton>
+      {/* <Button
+        mt="md"
+        mx="md"
+        component={Link}
+        href="#contact"
+        variant="default"
+        radius="xl"
+        size="md"
+        rightSection={<IconArrowRight size={16} />}
+      >
+        Connect
+      </Button> */}
+    </AppShell.Navbar>
+  );
+}
