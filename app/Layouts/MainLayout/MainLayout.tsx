0208c31351033db45c2e5c2c6f8377e85c69bcb3
hbich aymane <aymangame077@gmail.com>
2025-07-08 00:21:51 +0100

    feat : main layout has header and navbar

---
+++ app/Layouts/MainLayout/MainLayout.tsx
@@ -0,0 +1,28 @@
+"use client";
+import { AppShell, Burger, Button, Group, UnstyledButton } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import Header from "../../Components/Header/Header";
+import Navbar from "../../Components/Navbar/Navbar";
+export default function MainLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [opened, { toggle, close }] = useDisclosure();
+  return (
+    <AppShell
+      header={{ height: 70 }}
+      navbar={{
+        width: 300,
+        breakpoint: "md",
+        collapsed: { desktop: true, mobile: !opened },
+      }}
+      styles={{ header: { borderBottom: "none" } }}
+      padding="md"
+    >
+      <Header opened={opened} toggle={toggle} />
+      <Navbar />
+      <AppShell.Main>{children}</AppShell.Main>
+    </AppShell>
+  );
+}
