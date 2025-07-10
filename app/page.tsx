5dd6c40014c6cb585e0acf764bf24a58b2aaf14d
hbich aymane <aymangame077@gmail.com>
2025-07-10 21:37:49 +0100

    refactor : cleaning lines of code.

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,25 @@
+import { Container } from "@mantine/core";
+import Header from "./Components/Header/Header";
+import HeroSection from "./Components/Sections/HeroSection/HeroSection";
+import AboutSection from "./Components/Sections/AboutSection/AboutSection";
+import ExperiencesSection from "./Components/Sections/ExperiencesSection/ExperiencesSection";
+import EducationSection from "./Components/Sections/EducationSection/EducationSection";
+import ProjectsSection from "./Components/Sections/ProjectsSection/ProjectsSection";
+import SkillsSection from "./Components/Sections/SkillsSection/SkillsSection";
+import ContactSection from "./Components/Sections/Contact/ContactSection";
+import ContactSection_v2 from "./Components/Sections/Contact/ContactSection_v2";
+
 export default function HomePage() {
-  return <div>aymane hbich</div>;
+  return (
+    <Container size="lg" p={0}>
+      <HeroSection />
+      <AboutSection />
+      <SkillsSection />
+      <EducationSection />
+      <ExperiencesSection />
+      <ProjectsSection />
+      <ContactSection/>
+      {/* <ContactSection_v2 /> */}
+    </Container>
+  );
 }
