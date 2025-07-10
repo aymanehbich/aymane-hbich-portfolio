368a78cb54972359755c2183f459d036d03dbd37
hbich aymane <aymangame077@gmail.com>
2025-07-10 21:36:39 +0100

    feat : adding constants data for reasubality.

---
+++ app/constants.ts
@@ -0,0 +1,48 @@
+export const skillsData = [
+  {
+    category: "Frontend Development",
+    skills: [
+      { name: "HTML", level: 90 },
+      { name: "CSS", level: 85 },
+      { name: "JavaScript", level: 80 },
+      { name: "React JS", level: 75 },
+      { name: "Next JS", level: 70 },
+    ],
+  },
+  {
+    category: "Backend Development",
+    skills: [
+      { name: "Node.js", level: 70 },
+      { name: "Express.js", level: 65 },
+      { name: "Laravel", level: 60 },
+      { name: "MongoDB", level: 55 },
+    ],
+  },
+  {
+    category: "Tools & Technologies",
+    skills: [
+      { name: "Git & GitHub", level: 85 },
+      { name: "VS Code", level: 90 },
+      { name: "Postman", level: 75 },
+      { name: "Figma", level: 60 },
+    ],
+  },
+];
+export const skillsStats = [
+  {
+    value: "3+",
+    label: "Years of Experience",
+  },
+  {
+    value: "5+",
+    label: "Completed Projects",
+  },
+  {
+    value: "4",
+    label: "Technologies Mastered",
+  },
+  {
+    value: "2",
+    label: "Internships",
+  },
+];
