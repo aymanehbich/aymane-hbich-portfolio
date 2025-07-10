50bdac3bc4421a4cd4ea1b85eece4bb455f372a2
hbich aymane <aymangame077@gmail.com>
2025-07-11 00:04:24 +0100

    optimize : optimizing the css imports.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,45 @@
-import "@mantine/core/styles.css";
+// import "@mantine/core/styles.css";
+// Global styles (required for all Mantine components)
+import '@mantine/core/styles/baseline.css';
+import '@mantine/core/styles/default-css-variables.css';
+import '@mantine/core/styles/global.css';
+// Component-specific styles
+import '@mantine/core/styles/ScrollArea.css';
+import '@mantine/core/styles/UnstyledButton.css';
+import '@mantine/core/styles/VisuallyHidden.css';
+import '@mantine/core/styles/Paper.css';
+import '@mantine/core/styles/Popover.css';
+import '@mantine/core/styles/CloseButton.css';
+import '@mantine/core/styles/Loader.css';
+import '@mantine/core/styles/Overlay.css';
+import '@mantine/core/styles/ModalBase.css';
+import '@mantine/core/styles/Input.css';
+import '@mantine/core/styles/InlineInput.css';
+import '@mantine/core/styles/Flex.css';
+import '@mantine/core/styles/FloatingIndicator.css';
+import '@mantine/core/styles/ActionIcon.css';
+import '@mantine/core/styles/Avatar.css';
+import '@mantine/core/styles/Button.css';
+import '@mantine/core/styles/Container.css';
+import '@mantine/core/styles/Group.css';
+import '@mantine/core/styles/Text.css';
+import '@mantine/core/styles/Title.css';
+import '@mantine/core/styles/AppShell.css';
+import '@mantine/core/styles/Burger.css';
+import '@mantine/core/styles/Stack.css';
+import '@mantine/core/styles/Divider.css';
+import '@mantine/core/styles/Card.css';
+import '@mantine/core/styles/Badge.css';
+import '@mantine/core/styles/Anchor.css';
+import '@mantine/core/styles/Grid.css';
+import '@mantine/core/styles/SimpleGrid.css';
+import '@mantine/core/styles/List.css';
+import '@mantine/core/styles/Tooltip.css';
+import '@mantine/core/styles/Center.css';
+import '@mantine/core/styles/Image.css';
+import '@mantine/core/styles/AspectRatio.css';
+import '@mantine/core/styles/ThemeIcon.css';
+import '@mantine/core/styles/Progress.css';
 import React from "react";
 import {
   MantineProvider,
