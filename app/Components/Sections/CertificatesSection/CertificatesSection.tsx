2df66b2d31319eb905d7f36edf60b8c266d4a660
hbich aymane <aymangame077@gmail.com>
2025-07-13 00:34:21 +0100

    feat: add certificate section with style

---
+++ app/Components/Sections/CertificatesSection/CertificatesSection.tsx
@@ -0,0 +1,139 @@
+import {
+  Anchor,
+  Avatar,
+  Card,
+  Code,
+  Container,
+  Grid,
+  GridCol,
+  Group,
+  Image,
+  Paper,
+  Text,
+  Title,
+} from "@mantine/core";
+import classes from "./CertificatesSection.module.css";
+const certificates = [
+  {
+    title: "JavaScript Algorithms and Data Structures (Beta)",
+    issuer: "freeCodeCamp",
+    issued: "October 2024",
+    credentialId: "aymane_hbich-jaads",
+    link: "https://www.freecodecamp.org/certification/aymane_hbich/javascript-algorithms-and-data-structures",
+    skills: ["JavaScript", "Problem Solving"],
+    logo: "/logos/freecodecamp.jpeg",
+    pdf: "/certificates/javascript-certificate.pdf",
+  },
+  {
+    title: "Responsive Web Design",
+    issuer: "freeCodeCamp",
+    issued: "September 2024",
+    credentialId: "aymane_hbich-rwd",
+    link: "https://www.freecodecamp.org/certification/aymane_hbich/responsive-web-design",
+    skills: [
+      "Bootstrap (Framework)",
+      "Responsive Web Design",
+      "CSS",
+      "Responsiveness",
+      "HTML5",
+    ],
+    logo: "/logos/freecodecamp.jpeg",
+    pdf: "/certificates/responsive-web-design.pdf",
+  },
+  {
+    title: "Introduction to NoSQL",
+    issuer: "Great Learning",
+    issued: "October 2024",
+    credentialId: "VDOEUEFR",
+    link: "https://verify.mygreatlearning.com/",
+    skills: ["NoSQL", "MongoDB"],
+    logo: "/logos/great_learning.png",
+    pdf: "/certificates/nosql-intro.pdf",
+  },
+  {
+    title: "SQL and Relational Databases 101",
+    issuer: "IBM",
+    issued: "July 2024",
+    credentialId: "course-v1:BDU DB0101EN v1",
+    link: "https://courses.cognitiveclass.ai/certificates/9275465f50b84b188d6838d14f48b572",
+    skills: ["DBMS", "SQL", "Database Design", "Data Manipulation"],
+    logo: "/logos/ibm.jpeg",
+    pdf: "/certificates/sql-relational-db.pdf",
+  },
+];
+export default function CertificatesSection() {
+  return (
+    <Container id="certificates" fluid pb="xl" px={0} pt={80}>
+      <Paper maw={700} mx="auto" mb="xl">
+        <Title ta="center" order={2} fw={800} size="h1">
+          Certificates And Achievements
+        </Title>
+        <Text ta="center" c="gray.7" size="lg">
+          A collection of my professional certifications and notable
+          achievements that reflect my commitment to continuous learning and
+          excellence in software development.
+        </Text>
+      </Paper>
+      <Grid>
+        {certificates.map((cert, index) => (
+          <GridCol key={index} span={{ base: 12, md: 6, lg: 4 }}>
+            <Card
+              withBorder
+              radius="md"
+              // p="md"
+              className={classes.card}
+              h="100%"
+            >
+              <Text tt="uppercase" c="dimmed" fw={700} size="xs">
+                Certification
+              </Text>
+
+              <Text className={classes.title} mt="xs" mb="sm" fw={600}>
+                {cert.title}
+              </Text>
+              <Text size="sm" c="gray.7" mb="xs">
+                Issued by {cert.issuer} · <strong>{cert.issued}</strong>
+              </Text>
+              <Text size="sm" c="gray.7" mb="xs">
+                Credential ID:{" "}
+                <Code color="myColor" c="white">
+                  {cert.credentialId}
+                </Code>
+              </Text>
+              <Text size="sm" c="gray.7" mb="sm">
+                Skills: {cert.skills.join(" · ")}
+              </Text>
+              <div style={{ flexGrow: 1 }}></div>
+              <Group wrap="nowrap" justify="space-between" mt="sm">
+                <Group gap="xs" wrap="nowrap">
+                  <Avatar size={24} src={cert.logo} alt={cert.issuer} />
+                  <Text size="xs">{cert.issuer}</Text>
+                </Group>
+                <Group gap="xs">
+                  <Anchor
+                    href={cert.link}
+                    target="_blank"
+                    size="xs"
+                    fw={500}
+                    underline="always"
+                  >
+                    Show credential
+                  </Anchor>
+                  <Anchor
+                    href={cert.pdf}
+                    target="_blank"
+                    size="xs"
+                    fw={500}
+                    underline="always"
+                  >
+                    View PDF
+                  </Anchor>
+                </Group>
+              </Group>
+            </Card>
+          </GridCol>
+        ))}
+      </Grid>
+    </Container>
+  );
+}
