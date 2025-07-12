926ad41640ab1e94cb99f04a982b36af8edc9b35
hbich aymane <aymangame077@gmail.com>
2025-07-12 22:37:00 +0100

    update : changing from mantine image to next image

--- app/Components/Sections/HeroSection/HeroSection.tsx
+++ app/Components/Sections/HeroSection/HeroSection.tsx
@@ -1,18 +1,37 @@
-import { Avatar, Button, Container, Group, Text, Title } from "@mantine/core";
+import {
+  Avatar,
+  Button,
+  Container,
+  Group,
+  Paper,
+  Text,
+  Title,
+} from "@mantine/core";
 import React from "react";
 import { IconArrowUpRight, IconDownload } from "@tabler/icons-react";
 import classes from "./Hero.module.css";
+import Image from "next/image";
+import ProfilePic from "../../../../public/HBICH_AYMANE.png";
 export default function HeroSection() {
   return (
     <Container id="hero" size="lg" pb="xl" maw={900} px={0} pt={40}>
-      <Avatar
-        src="/HBICH_AYMANE.png"
-        radius={150}
-        size={150}
+      <Paper
+        w={150}
+        h={150}
         mx="auto"
+        radius={150}
         bd="2px dashed gray.4"
         mb="md"
-      />
+        style={{ overflow: "hidden" }}
+      >
+        <Image
+          src={ProfilePic}
+          alt="Aymane Hbich"
+          width={146}
+          height={146}
+          loading="lazy"
+        />
+      </Paper>
       <Text ff="--font-my" fz={22} fw={500} ta="center">
         Hi! I&apos;m aymane hbich👋
       </Text>
