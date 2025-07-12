9aedb707b056f37ddd46bcf6f08e42ba90981cda
hbich aymane <aymangame077@gmail.com>
2025-07-12 12:13:32 +0100

    optimize : seo optimization.

--- app/layout.tsx
+++ app/layout.tsx
@@ -93,7 +93,6 @@ export default function RootLayout({ children }: { children: any }) {
     <html lang="en" className={myFont.variable} {...mantineHtmlProps}>
       <head>
         <ColorSchemeScript />
-        <link rel="shortcut icon" href="/favicon.png" />
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
