ca98ba3a55adcaed98ac94efcc113a7468acc866
hbich aymane <aymangame077@gmail.com>
2025-07-14 13:28:23 +0100

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -4,20 +4,20 @@ const nextConfig = {
   experimental: {
     optimizePackageImports: ["@mantine/core", "@mantine/hooks"],
   },
-  async headers() {
-    return [
-      {
-        // Cache all images, JS, CSS, fonts for 1 year
-        source: "/(.*)\\.(js|css|webp|png|jpg|jpeg|svg|woff2?)",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "public, max-age=31536000, immutable",
-          },
-        ],
-      },
-    ];
-  },
+  // async headers() {
+  //   return [
+  //     {
+  //       // Cache all images, JS, CSS, fonts for 1 year
+  //       source: "/(.*)\\.(js|css|webp|png|jpg|jpeg|svg|woff2?)",
+  //       headers: [
+  //         {
+  //           key: "Cache-Control",
+  //           value: "public, max-age=31536000, immutable",
+  //         },
+  //       ],
+  //     },
+  //   ];
+  // },
 };
 
 export default nextConfig;
