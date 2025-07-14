023a1c78e59eff6f4b3851f341efbd352d44faf7
hbich aymane <aymangame077@gmail.com>
2025-07-14 14:16:52 +0100

    fix(performance) : performance issues

--- next.config.mjs
+++ next.config.mjs
@@ -1,38 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  compress: false,
   reactStrictMode: true,
   experimental: {
     optimizePackageImports: ["@mantine/core", "@mantine/hooks"],
   },
   async headers() {
     return [
-      // Aggressively cache static assets (1 year, immutable)
-      {
-        source: "/_next/static/(.*)", // Next.js build assets
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "public, max-age=31536000, immutable",
-          },
-        ],
-      },
       {
         source: "/(.*)\\.(webp|png|jpg|jpeg|svg|woff2?|css|js)",
         headers: [
           {
             key: "Cache-Control",
             value: "public, max-age=31536000, immutable",
-          },
-        ],
-      },
-      // HTML should not be cached for too long
-      {
-        source: "/",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "no-cache, no-store, must-revalidate",
           },
         ],
       },
